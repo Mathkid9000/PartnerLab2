3cf51707074840454a38fc4a7d9f6c53aa0d431e
Mathk <leemumc@gmail.com>
2025-10-11 21:29:27 -0700

    Created random grid w images

--- PartnerLab2/ContentView.swift
+++ PartnerLab2/ContentView.swift
@@ -8,14 +8,45 @@
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+    
+    func generateTiles() -> [Int] {
+        var possibleTiles: [Int] = Array(1..<12)
+        var selectedTileTypes: [Int] = []
+        for _ in 0..<8 {
+            let selection = Int.random(in: 0..<possibleTiles.count)
+            selectedTileTypes.append(possibleTiles[selection])
+            possibleTiles.remove(at: selection)
         }
-        .padding()
+        var tiles: [Int] = []
+        for _ in 0..<16 {
+            let selection = Int.random(in: 0..<selectedTileTypes.count)
+            let tileType = selectedTileTypes[selection]
+            if tiles.contains(tileType) {
+                selectedTileTypes.remove(at: selection)
+            }
+            tiles.append(tileType)
+        }
+        return tiles
+    }
+    
+    var body: some View {
+        let tiles = generateTiles()
+        
+        Text("Memory Game")
+            .font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/)
+            .fontWeight(/*@START_MENU_TOKEN@*/.bold/*@END_MENU_TOKEN@*/)
+        LazyHGrid(rows: [GridItem(.fixed(80)),GridItem(.fixed(80)),GridItem(.fixed(80)),GridItem(.fixed(80))]) {
+            ForEach(0..<16) { i in
+                VStack{
+                    Image("\(tiles[i])").resizable()
+                        .frame(width:50,height:50)
+                        .padding(15)
+                        .onTapGesture {
+                            print("Card tapped")
+                        }
+                }
+            }
+        }
     }
 }
 
