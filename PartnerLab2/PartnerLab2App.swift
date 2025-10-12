33f4b45a144ac0c3afb19f2a6891f096b4dd9b30
Mathk <leemumc@gmail.com>
2025-10-11 20:36:38 -0700

    Initial Commit

---
+++ PartnerLab2/PartnerLab2App.swift
@@ -0,0 +1,17 @@
+//
+//  PartnerLab2App.swift
+//  PartnerLab2
+//
+//  Created by Mac User on 10/11/25.
+//
+
+import SwiftUI
+
+@main
+struct PartnerLab2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
