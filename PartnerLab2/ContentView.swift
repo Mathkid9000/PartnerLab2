28c34e9dd78a5c33b9199ce501e84b2b5454c543
Mathk <leemumc@gmail.com>
2025-10-14 23:15:39 -0700

    Finished game logic

--- PartnerLab2/ContentView.swift
+++ PartnerLab2/ContentView.swift
@@ -1,7 +1,8 @@
 //
 //  ContentView.swift
 //
-//  Assignment: PartnerLab2
+//  Partner Lab 2
+//  Group # 12
 //  Created by:
 //              Liam Christensen
 //              Edgar Rosales
@@ -10,9 +11,11 @@
 
 import SwiftUI
 
-struct ContentView: View {
+class MemoryGame: ObservableObject {
+    public static var _instance: MemoryGame? = nil
+    
     // Logic for generating a random board of paired tiles
-    func generateTiles() -> [Int] {
+    static func generateTiles() -> [Int] {
         var possibleTiles: [Int] = Array(1..<12)
         var selectedTileTypes: [Int] = []
         for _ in 0..<8 {
@@ -20,41 +23,60 @@ struct ContentView: View {
             selectedTileTypes.append(possibleTiles[selection])
             possibleTiles.remove(at: selection)
         }
-        var tiles: [Int] = []
+        var _tiles: [Int] = []
         for _ in 0..<16 {
             let selection = Int.random(in: 0..<selectedTileTypes.count)
             let tileType = selectedTileTypes[selection]
-            if tiles.contains(tileType) {
+            if _tiles.contains(tileType) {
                 selectedTileTypes.remove(at: selection)
             }
-            tiles.append(tileType)
+            _tiles.append(tileType)
         }
-        return tiles
+        return _tiles
     }
 
-    static var tileUp: CardView? = nil // current flipped card -> nil if no card is flipped over
-    static var completePairs: Int = 0 // amount of correctly matched pairs
-    static var isProcessingClick: Bool = false // small time allocated for showing second card in pair
-    var body: some View {
-        let tiles = generateTiles()
-        
-        Text("Memory Game")
-            .font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/)
-            .fontWeight(/*@START_MENU_TOKEN@*/.bold/*@END_MENU_TOKEN@*/)
-        LazyHGrid(rows:
-                    [GridItem(.adaptive(minimum: 40, maximum: .infinity)),
-                     GridItem(.adaptive(minimum: 40, maximum: .infinity)),
-                     GridItem(.adaptive(minimum: 40, maximum: .infinity)),
-                     GridItem(.adaptive(minimum: 40, maximum: .infinity))],
-                  spacing: 0
-        )
-        {
-            ForEach(0..<16) { i in
-                CardView (image: "\(tiles[i])", id_index: i)
-                    .frame(minWidth: 40, maxWidth: .infinity)
-            }
-        }.padding(0)
+    var tileUp: CardView? = nil // current flipped card -> nil if no card is flipped over
+    @Published var tiles: [Int]
+    @Published var completePairs: Int = 0
+    var isProcessingClick: Bool = false // small time allocated for showing second card in pair
+    
+    init() {
+        self.tiles = MemoryGame.generateTiles()
+        MemoryGame._instance = self
     }
+}
+
+struct ContentView: View {
+    @StateObject private var game = MemoryGame()
+    
+    var body: some View {
+        VStack {
+            Text("Matched Pairs: \(game.completePairs)/8")
+            Text("Memory Game")
+                .font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/)
+                .fontWeight(/*@START_MENU_TOKEN@*/.bold/*@END_MENU_TOKEN@*/)
+            LazyHGrid(rows:
+                        [GridItem(.adaptive(minimum: 40, maximum: .infinity)),
+                         GridItem(.adaptive(minimum: 40, maximum: .infinity)),
+                         GridItem(.adaptive(minimum: 40, maximum: .infinity)),
+                         GridItem(.adaptive(minimum: 40, maximum: .infinity))],
+                      spacing: 0
+            )
+            {
+                ForEach(0..<16) { i in
+                    CardView (image: "\(game.tiles[i])", id_index: i)
+                        .frame(minWidth: 40, maxWidth: .infinity)
+                }
+            }.padding(0)
+            
+            if game.completePairs == 8 {
+                Text("You Won!")
+                    .font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/)
+                    .padding(20)
+            }
+        }
+    }
+
 }
 
 // Implements functionality to cover and uncover cars upon tap gesture
@@ -65,35 +87,41 @@ struct CardView: View {
     @State var isFaceUp: Bool = false // false: Cards covered by default, true: cards uncovered by default
     @State var isComplete: Bool = false // false: Cards have no effect, true: add an opacity effect to show that it has been paired
     
+    init(image: String, id_index: Int) {
+        self.image = image
+        self.id_index = id_index
+    }
+    
     // Logic for when a card is flipped over
     func flipCard() {
-        if ContentView.isProcessingClick == true {
+        if MemoryGame._instance?.isProcessingClick == true || isComplete {
             return
         }
         
-        if ContentView.tileUp == nil {
-            ContentView.tileUp = self
+        if MemoryGame._instance?.tileUp == nil {
+            MemoryGame._instance?.tileUp = self
             isFaceUp = true
         }
         else {
-            if ContentView.tileUp!.id_index == id_index {
+            if MemoryGame._instance?.tileUp!.id_index == id_index {
                 return
             }
 
             isFaceUp = true
-            if ContentView.tileUp!.image == image {
-                ContentView.completePairs += 1
+            if MemoryGame._instance?.tileUp!.image == image {
+                MemoryGame._instance?.completePairs += 1
+                print(MemoryGame._instance?.completePairs)
                 isComplete = true
-                ContentView.tileUp?.isComplete = true
-                ContentView.tileUp = nil
+                MemoryGame._instance?.tileUp?.isComplete = true
+                MemoryGame._instance?.tileUp = nil
             }
             else {
-                ContentView.isProcessingClick = true
+                MemoryGame._instance?.isProcessingClick = true
                 DispatchQueue.main.asyncAfter(deadline: .now() + 1.0) {
-                    ContentView.tileUp!.isFaceUp = false
+                    MemoryGame._instance?.tileUp!.isFaceUp = false
                     isFaceUp = false
-                    ContentView.tileUp = nil
-                    ContentView.isProcessingClick = false
+                    MemoryGame._instance?.tileUp = nil
+                    MemoryGame._instance?.isProcessingClick = false
                 }
             }
         }
@@ -104,7 +132,6 @@ struct CardView: View {
             let shape = ZStack {
                 RoundedRectangle(cornerSize: CGSize(width: 6, height: 6))
                     .foregroundColor(.white)
-                    .shadow(color: .black, radius: 0, y: 4)
                     .border(Color.black, width: 1)
                     .frame(width: 60, height: 60)
                     .padding(0)
@@ -132,7 +159,7 @@ struct CardView: View {
             else {
                 shape.opacity(1)
             }
-        }.padding()
+        }.padding(5)
             .onTapGesture(perform: flipCard)
         
     }
